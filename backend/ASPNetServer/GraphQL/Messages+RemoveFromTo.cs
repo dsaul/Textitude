aefe1410857825a13cccee5897750badad6dc8b2
Dan <dan@dsaul.ca>
2023-06-05 21:31:54 -0500

    add removing conversations

---
+++ backend/ASPNetServer/GraphQL/Messages+RemoveFromTo.cs
@@ -0,0 +1,51 @@
+﻿using GraphQL.AspNet.Controllers;
+using GraphQL.AspNet.Attributes;
+using ASPNetServer.Mongo;
+using MongoDB.Driver;
+using Amazon.S3.Model;
+using DanSaul.SharedCode.StandardizedEnvironmentVariables;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Org.BouncyCastle.Utilities.Zlib;
+
+namespace ASPNetServer.GraphQL
+{
+	public partial class Messages : GraphController
+	{
+		[Mutation("removeFromTo")]
+		public GenericReturn RemoveFromTo(string e164)
+		{
+			IEnumerable<MessageDocument> messages = from message in MessageDocuments.AsQueryable()
+													where message.To == e164 || message.From == e164
+													select message;
+
+			foreach (MessageDocument message in messages)
+			{
+				// If there are any stored attachments, delete them.
+				List<MessageAttachment> attachments = message.Attachments;
+				if (attachments.Any())
+				{
+					foreach (MessageAttachment attachment in attachments)
+					{
+						DeleteObjectRequest request = new()
+						{
+							BucketName = EnvAmazonS3.S3_BUCKET_MMS,
+							Key = attachment.Key,
+						};
+						S3Client.DeleteObjectAsync(request).Wait();
+					}
+				}
+
+				var filter = Builders<MessageDocument>.Filter.Eq(entry => entry.Id, message.Id);
+				MessageDocuments.DeleteOne(filter);
+			}
+
+
+
+
+			return new GenericReturn()
+			{
+				Status = "success",
+			};
+		}
+	}
+}
--- backend/ASPNetServer/GraphQL/Messages.cs
+++ backend/ASPNetServer/GraphQL/Messages.cs
@@ -1,4 +1,5 @@
 ﻿// (c) 2023 Dan Saul
+using Amazon.S3;
 using ASPNetServer.Mongo;
 using GraphQL.AspNet.Attributes;
 using GraphQL.AspNet.Controllers;
@@ -14,11 +15,17 @@ namespace ASPNetServer.GraphQL
 		IMongoCollection<MessageDocument> MessageDocuments { get; init; }
 		[GraphSkip]
 		SpamFilterController SpamFilterController { get; init; }
+		AmazonS3Client S3Client { get; init; }
 
-		public Messages(IMongoCollection<MessageDocument> _MessageDocuments, SpamFilterController _SpamFilterController)
+		public Messages(
+			IMongoCollection<MessageDocument> _MessageDocuments, 
+			SpamFilterController _SpamFilterController,
+			AmazonS3Client _S3Client
+			)
 		{
 			MessageDocuments = _MessageDocuments;
 			SpamFilterController = _SpamFilterController;
+			S3Client = _S3Client;
 		}
 
 
